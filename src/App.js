948a67c8b0d02a62f12b422c2b8945b3965bfe6e
Matt Ginter <mattginter@gmail.com>
2022-04-04 19:22:31 +0200

    adds main views

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react"
-import Home from "./views/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./views/Home/Home"
+import Contact from "./views/Contact/Contact"
+import Company from "./views/Company/Company"
+import Navigation from "./components/Navigation"
 
 const App = () => {
-  return <Home />
+  return (
+    <Router>
+      <Navigation />
+      <Home />
+      <Routes>
+        <Route path="home" element={Home} />
+        <Route path="contact" element={Contact} />
+        <Route path=":company" element={Company} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <>
+      <div>Remote Job Finder</div>
+      <input type="text" />
+    </>
+  )
+}
+
+export default Home
